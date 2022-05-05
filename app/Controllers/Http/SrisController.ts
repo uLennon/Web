6cced848ddd4f2bd5496ef34c2b97a81a5f156f5
Lennon <lennonemp@gmail.com>
2022-05-05 02:57:57 -0300

    configuração das paginas

--- app/Controllers/Http/SrisController.ts
+++ app/Controllers/Http/SrisController.ts
@@ -1,5 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
+import user from 'App/Models/user'
 
 export default class SrisController {
 
@@ -12,10 +12,8 @@ export default class SrisController {
         return view.render('principal')
     }
 
-    public async reporte({view,auth}: HttpContextContract){
-        await auth.use('web').authenticate()
-        
-        console.log(auth.user!)
+    public async reporte({view}: HttpContextContract){
+      
         return view.render('reporte')
     }
 
@@ -30,15 +28,34 @@ export default class SrisController {
         
         console.log(email)
         console.log(password)
+        
+        const use = await user.create({email, password})
 
         try{
-            await auth.use('web').attempt(email, password)
+            await auth.use('web').login(use)
             response.redirect().toRoute('principal')
         }catch {
             response.badRequest('invalido')
         }
     }
     
+    public async estore({request, response,auth}: HttpContextContract){
+    
+        const email = request.input('email')
+        const password = request.input('password')
+        
+        console.log(email)
+        console.log(password)
+        
+        const use = await user.create(email,password)
+
+        try{
+            await auth.use('web').attempt(email,password)
+            response.redirect().toRoute('principal')
+        }catch {
+            response.badRequest('invalido')
+        }
+    }
     public async delete({response, auth}: HttpContextContract){
         await auth.use('web').logout()
         return response.redirect().toRoute('login')
--- start/routes.ts
+++ start/routes.ts
@@ -22,8 +22,14 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/login','SrisController.login').as('login')
 Route.post('/login','SrisController.store').as('store')
-Route.get('/principal','SrisController.principal').as('principal').middleware('auth')
-Route.get('/reporte','SrisController.reporte').as('reporte')
+Route.group(()=>{
+    Route.get('/principal','SrisController.principal').as('principal')
+    Route.get('/reporte','SrisController.reporte').as('reporte')
+}).middleware('auth')
+
+
+
 Route.get('/registro','SrisController.registro').as('registro')
-Route.post('/registro','SrisController.registro').as('storee')
+Route.post('/registro','SrisController.estore').as('estore')
+
 Route.get('/logout','SrisController.delete').as('delete')
